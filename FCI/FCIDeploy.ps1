46b199466997294e745e9ce75b51ddcf05b764c9
Troy Ault <aultt@users.noreply.github.com>
2018-04-25 15:22:28 -0400

    update to dsc runas

--- FCI/FCIDeploy.ps1
+++ FCI/FCIDeploy.ps1
@@ -2,8 +2,8 @@ Login-AzureRmAccount
 Select-AzureRmSubscription -SubscriptionName TAMZ_SandBox
 
 $RG = 'sqlfci06'
-$templateFile = 'C:\Users\troyault.TAMZ\OneDrive - TAMZ\Git\ARM\FCI\azuredeploy.json'
-$templateParm = 'C:\Users\troyault.TAMZ\OneDrive - TAMZ\Git\MyParmFiles\FCI.parameters.json' 
+$templateFile = 'D:\Users\TroyAult\OneDrive - TAMZ\Git\ARM\FCI\azuredeploy.json'
+$templateParm = 'D:\Users\TroyAult\OneDrive - TAMZ\Git\MyParmFiles\FCI.parameters.json' 
 New-AzureRmResourceGroup -Name $RG -Location "East US"
 New-AzureRmResourceGroupDeployment -Name NewFCI -ResourceGroupName $RG -TemplateFile $templateFile -TemplateParameterFile $templateParm  -Verbose
 
