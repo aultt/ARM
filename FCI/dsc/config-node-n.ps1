cb2ea5b6f8599cd4962a698e11b5f228e544c871
Troy Ault <troyault@tamz.us>
2018-04-30 23:13:59 -0400

    Update to N nodes

--- FCI/dsc/config-node-n.ps1
+++ FCI/dsc/config-node-n.ps1
@@ -8,39 +8,29 @@ configuration ConfigNodeN
         [System.Management.Automation.PSCredential]$domainuserCreds,
         [Parameter(Mandatory)]
         [System.Management.Automation.PSCredential]$svcCreds,      
-        [Parameter(Mandatory)]
-        [String]$ClusterName,
-        [Parameter(Mandatory)]
-        [String]$SQLClusterName,
-        [Parameter(Mandatory)]
-        [String]$witnessStorageName,
-        [Parameter(Mandatory)]
-        [System.Management.Automation.PSCredential]$witnessStorageKey,
-        [Parameter(Mandatory)]
-        [String]$clusterIP,
         
         [Int]$RetryCount = 20,
-        [Int]$RetryIntervalSec = 30,
-        [Int]$probePort = 37000, 
+        [Int]$RetryIntervalSec = 30
+        #[Int]$probePort = 37000, 
 
-        [string]$datadriveLetter,
-        [string]$datadrivelabel,
-        [string]$datadriveSize,
-        [string]$logdriveLetter,
-        [string]$logdrivelabel,
-        [string]$logdriveSize,
-        [string]$tempdbdriveLetter,
-        [string]$tempdbdrivelabel,
-        [string]$tempdbdriveSize,
-        [string]$SQLFeatures,
-        [string]$SQLInstance,
-        [string]$InstallSQLDataDir,
-        [string]$InstanceDir ="G:\",
-        [string]$SQLUserDBDir = "G:\MSSQL",
-        [string]$SQLUserDBLogDir = "L:\MSSQL",
-        [string]$SQLTempDBDir = "T:\TEMPDB",
-        [string]$SQLTempDBLogDir = "T:\TEMPDB",
-        [string]$SQLBackupDir = "G:\BACKUP"
+        #[string]$datadriveLetter,
+        #[string]$datadrivelabel,
+        #[string]$datadriveSize,
+        #[string]$logdriveLetter,
+        #[string]$logdrivelabel,
+        #[string]$logdriveSize,
+        #[string]$tempdbdriveLetter,
+        #[string]$tempdbdrivelabel,
+        #[string]$tempdbdriveSize,
+        #[string]$SQLFeatures,
+        #[string]$SQLInstance,
+        #[string]$InstallSQLDataDir,
+        #[string]$InstanceDir ="G:\",
+        #[string]$SQLUserDBDir = "G:\MSSQL",
+        #[string]$SQLUserDBLogDir = "L:\MSSQL",
+        #[string]$SQLTempDBDir = "T:\TEMPDB",
+        #[string]$SQLTempDBLogDir = "T:\TEMPDB",
+        #[string]$SQLBackupDir = "G:\BACKUP"
     )
 
     Import-DscResource -ModuleName xComputerManagement, xActiveDirectory, SQLServerDSC, xPendingReboot, xNetworking,xFailoverCluster
