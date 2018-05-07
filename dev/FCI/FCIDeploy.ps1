3472058082e76db1616adf4a41bb228710f4c67b
Troy Ault <troyault@tamz.us>
2018-05-07 16:14:29 -0400

    correction

--- dev/FCI/FCIDeploy.ps1
+++ dev/FCI/FCIDeploy.ps1
@@ -2,7 +2,7 @@ Login-AzureRmAccount
 Select-AzureRmSubscription -SubscriptionName TAMZ_SandBox
 
 $RG = 'sqlfci06'
-$templateFile = 'C:\Users\troyault\OneDrive - TAMZ\Git\ARM\FCI\azuredeploy.json'
+$templateFile = 'C:\Users\troyault\OneDrive - TAMZ\Git\ARM\dev\FCI\azuredeploy.json'
 $templateParm = 'C:\Users\troyault\OneDrive - TAMZ\Git\MyParmFiles\FCI.parameters.json' 
 New-AzureRmResourceGroup -Name $RG -Location "East US"
 New-AzureRmResourceGroupDeployment -Name NewFCI -ResourceGroupName $RG -TemplateFile $templateFile -TemplateParameterFile $templateParm  -Verbose
