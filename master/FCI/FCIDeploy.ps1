f97cf004921ae969e0b40b9dae4b801ad1bfb804
Troy Ault <troyault@tamz.us>
2018-05-07 14:50:20 -0400

    Rolling Back to NewerFCI

---
+++ master/FCI/FCIDeploy.ps1
@@ -0,0 +1,13 @@
+Login-AzureRmAccount
+Select-AzureRmSubscription -SubscriptionName TAMZ_SandBox
+
+$RG = 'sqlfci06'
+$templateFile = 'C:\Users\troyault\OneDrive - TAMZ\Git\ARM\FCI\azuredeploy.json'
+$templateParm = 'C:\Users\troyault\OneDrive - TAMZ\Git\MyParmFiles\FCI.parameters.json' 
+New-AzureRmResourceGroup -Name $RG -Location "East US"
+New-AzureRmResourceGroupDeployment -Name NewFCI -ResourceGroupName $RG -TemplateFile $templateFile -TemplateParameterFile $templateParm  -Verbose
+
+Remove-AzureRmResourceGroup -Name $RG -Force
+
+
+
