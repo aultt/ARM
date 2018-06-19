a12a5225646f4ebfe33f35d4a5401ed96ddaef4e
Troy Ault <troyault@tamz.us>
2018-06-19 10:44:09 -0400

    Removing unneeded ,

--- SQLStandAlone/dsc/Standalone.ps1
+++ SQLStandAlone/dsc/Standalone.ps1
@@ -64,7 +64,7 @@ $SQLVersion = 'MSQL14'
             SetScript  = 
 @"                      
                         New-StoragePool -FriendlyName 'SQLPool' -StorageSubSystemFriendlyName "Windows Storage*" -PhysicalDisks (Get-PhysicalDisk -canpool:1) 
-                        New-Volume -StoragePoolFriendlyName SQLPool* -FriendlyName $datadrivelabel -FileSystem NTFS -AllocationUnitSize 65536 -DriveLetter $datadriveletter -size $datadriveSize,;
+                        New-Volume -StoragePoolFriendlyName SQLPool* -FriendlyName $datadrivelabel -FileSystem NTFS -AllocationUnitSize 65536 -DriveLetter $datadriveletter -size $datadriveSize;
                         New-Volume -StoragePoolFriendlyName SQLPool* -FriendlyName $logdrivelabel -FileSystem NTFS -AllocationUnitSize 65536 -DriveLetter $logdriveletter -size $logdrivesize;
                         New-Volume -StoragePoolFriendlyName SQLPool* -FriendlyName $tempdbdrivelabel -FileSystem NTFS -AllocationUnitSize 65536 -DriveLetter $tempdbdriveletter -size $tempdbdriveSize;
 "@
@@ -81,12 +81,12 @@ $SQLVersion = 'MSQL14'
             InstallSharedDir      = 'C:\Program Files\Microsoft SQL Server'
             InstallSharedWOWDir   = 'C:\Program Files (x86)\Microsoft SQL Server'
             InstanceDir           = 'G:\Program Files\Microsoft SQL Server'
-            InstallSQLDataDir     = "G:\Program Files\Microsoft SQL Server\MSSQL13.$SQLInstanceName\MSSQL\Data"
-            SQLUserDBDir          = "G:\Program Files\Microsoft SQL Server\MSSQL13.$SQLInstanceName\MSSQL\Data"
-            SQLUserDBLogDir       = "F:\Program Files\Microsoft SQL Server\MSSQL13.$SQLInstanceName\MSSQL\Data"
-            SQLTempDBDir          = "T:\Program Files\Microsoft SQL Server\MSSQL13.$SQLInstanceName\MSSQL\Data"
-            SQLTempDBLogDir       = "T:\Program Files\Microsoft SQL Server\MSSQL13.$SQLInstanceName\MSSQL\Data"
-            SQLBackupDir          = "G:\Program Files\Microsoft SQL Server\MSSQL13.$SQLInstanceName\MSSQL\Backup"
+            InstallSQLDataDir     = "G:\Program Files\Microsoft SQL Server\MSSQL14.$SQLInstanceName\MSSQL\Data"
+            SQLUserDBDir          = "G:\Program Files\Microsoft SQL Server\MSSQL14.$SQLInstanceName\MSSQL\Data"
+            SQLUserDBLogDir       = "F:\Program Files\Microsoft SQL Server\MSSQL14.$SQLInstanceName\MSSQL\Data"
+            SQLTempDBDir          = "T:\Program Files\Microsoft SQL Server\MSSQL14.$SQLInstanceName\MSSQL\Data"
+            SQLTempDBLogDir       = "T:\Program Files\Microsoft SQL Server\MSSQL14.$SQLInstanceName\MSSQL\Data"
+            SQLBackupDir          = "G:\Program Files\Microsoft SQL Server\MSSQL14.$SQLInstanceName\MSSQL\Backup"
             SourcePath            = 'C:\SQLServerFull'
             UpdateEnabled         = 'False'
             ForceReboot           = $false
