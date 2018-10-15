adc029d1a27cbb95ada6ce2badeecfe9ab471cf3
Troy Ault <aultt@hotmail.com>
2018-10-15 16:24:16 -0400

    Putting back AlwaysOnArm and commenting out DSC

--- FCI/FCIDeploy.ps1
+++ FCI/FCIDeploy.ps1
@@ -2,7 +2,7 @@ Login-AzureRmAccount
 
 Select-AzureRmSubscription -SubscriptionName TAMZ_InternalApps
 
-$RG = 'yourrg'
+$RG = 'MYFCI'
 New-AzureRmResourceGroup -Name $RG -Location "East US"
 New-AzureRmResourceGroupDeployment -Name NewFCI -ResourceGroupName $RG -TemplateFile 'C:\Repos\ARM-Templates\FCI\azuredeploy.json' `
  -TemplateParameterFile 'C:\Repos\ARM-Templates\FCI\azuredeploy.parameters.json'
