ac99c9650db527cf863f4130e7b38d59032c864c
Troy Ault <aultt@users.noreply.github.com>
2018-11-29 10:23:02 -0500

    AlwaysOnAddition LatestDSC
    Removing LinkedTemplates and leveraging latest DSC templates.  xFailoverCluster has custom changes to allow for Azure Compatability.
    This is initial build testing to commence

---
+++ AlwaysOnExistingVnet/deploy.ps1
@@ -0,0 +1,26 @@
+$resourceGroupName = 'ClusterTestingWithFC'
+$resourceGroupLocation = "East US"
+$templateFile = 'D:\GitHub\ARM\AlwaysOnExistingVnet\template.json'
+$templateParm = 'D:\GitHub\ParameterFiles\AlwaysOnExitingVNetparameters.json'
+
+Import-Module Az
+Login-AzAccount 
+Select-AZSubscription -SubscriptionName TAMZ_MS 
+
+#Create or check for existing resource group
+$resourceGroup = Get-AZResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+if(!$resourceGroup)
+{
+    Write-Host "Resource group '$resourceGroupName' does not exist. To create a new resource group, please enter a location.";
+    if(!$resourceGroupLocation) {
+        $resourceGroupLocation = Read-Host "resourceGroupLocation";
+    }
+    Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
+    New-AZResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
+}
+else{
+    Write-Host "Using existing resource group '$resourceGroupName'";
+}
+
+# Start the deployment
+New-AZResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFile -TemplateParameterFile $templateParm  -Verbose;
---
+++ AlwaysOnExistingVnet/dsc/PrimarySQLNode.ps1
@@ -0,0 +1,207 @@
+configuration AlwaysOnSQLServer
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds,
+
+        #[Parameter(Mandatory)]
+        #[System.Management.Automation.PSCredential]$SQLServicecreds,
+        [string]$imageoffer = "SQL2016-WS2016",
+        [string]$SQLFeatures = "SQLENGINE",
+        [string]$SQLInstanceName = "MSSQLSERVER",
+        [string]$datadriveLetter = 'C',
+        [string]$datadrivelabel,
+        [string]$datadriveSize,
+        [string]$logdriveLetter = 'C',
+        [string]$logdrivelabel,
+        [string]$logdriveSize,
+        [string]$tempdbdriveLetter = 'D',
+        [string]$tempdbdrivelabel,
+        [string]$tempdbdriveSize,
+        
+        [Int]$RetryCount = 20,
+        [Int]$RetryIntervalSec = 30
+
+    )
+
+    Import-DscResource -ModuleName ComputerManagementdsc,sqlserverdsc,xFailOverCluster
+    [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ($Admincreds.UserName, $Admincreds.Password)
+    [System.Management.Automation.PSCredential]$DomainFQDNCreds = New-Object System.Management.Automation.PSCredential ($Admincreds.UserName, $Admincreds.Password)
+    [System.Management.Automation.PSCredential]$SQLCreds = New-Object System.Management.Automation.PSCredential ($Admincreds.UserName, $Admincreds.Password)
+
+    $SQLVersion = $imageoffer.Substring(5,2)
+    $SQLLocation = "MSSQL$(switch ($SQLVersion){17 {14} 16 {13}})"
+    
+    WaitForSqlSetup
+
+    Node localhost
+    {
+
+        WindowsFeature FC
+        {
+            Name = "Failover-Clustering"
+            Ensure = "Present"
+        }
+
+		WindowsFeature FailoverClusterTools 
+        { 
+            Ensure = "Present" 
+            Name = "RSAT-Clustering-Mgmt"
+			DependsOn = "[WindowsFeature]FC"
+        } 
+
+        WindowsFeature FCPS
+        {
+            Name = "RSAT-Clustering-PowerShell"
+            Ensure = "Present"
+        }
+
+        WindowsFeature ADPS
+        {
+            Name = "RSAT-AD-PowerShell"
+            Ensure = "Present"
+        }
+        
+        xCluster CreateCluster
+        {
+            Name                          = 'MyLatestAttempt'
+            StaticIPAddress               = '10.50.2.51/24'
+            DomainAdministratorCredential = $DomainCreds
+            DependsOn                     = '[Computer]DomainJoin'
+        }
+
+        Computer DomainJoin
+        {
+            Name = $env:COMPUTERNAME
+            DomainName = $DomainName
+            Credential = $AdminCreds
+        }
+
+        PowerPlan HighPerf
+        {
+          IsSingleInstance = 'Yes'
+          Name             = 'High performance'
+        }
+
+        TimeZone TimeZoneExample
+        {
+            IsSingleInstance = 'Yes'
+            TimeZone         = 'Eastern Standard Time'
+        }
+
+
+        LocalConfigurationManager 
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        Script CleanSQL
+        {
+            SetScript  = 'C:\SQLServerFull\Setup.exe /Action=Uninstall /FEATURES=SQL,AS,RS,IS /INSTANCENAME=MSSQLSERVER /Q'
+            TestScript = "(test-path -Path `"C:\Program Files\Microsoft SQL Server\$SQLLocation.MSSQLSERVER\MSSQL\DATA\master.mdf`") -eq `$false"
+            GetScript  = "@{Ensure = if ((test-path -Path `"C:\Program Files\Microsoft SQL Server\$SQLLocation.MSSQLSERVER\MSSQL\DATA\master.mdf`") -eq `$false) {'Present'} Else {'Absent'}}"
+        }
+
+        SqlSetup 'InstallNamedInstance'
+        {
+            InstanceName          = $SQLInstanceName
+            Features              = $SQLFeatures
+            SQLCollation          = 'SQL_Latin1_General_CP1_CI_AS'
+            SQLSysAdminAccounts   = 'TAMZ\DBA'
+            InstallSharedDir      = 'C:\Program Files\Microsoft SQL Server'
+            InstallSharedWOWDir   = 'C:\Program Files (x86)\Microsoft SQL Server'
+            InstanceDir           = "${datadriveletter}:\Program Files\Microsoft SQL Server"
+            InstallSQLDataDir     = "${datadriveletter}:\Program Files\Microsoft SQL Server\$SQLLocation.$SQLInstanceName\MSSQL\"
+            SQLUserDBDir          = "${datadriveletter}:\Program Files\Microsoft SQL Server\$SQLLocation.$SQLInstanceName\MSSQL\Data"
+            SQLUserDBLogDir       = "${logdriveletter}:\Program Files\Microsoft SQL Server\$SQLLocation.$SQLInstanceName\MSSQL\Log"
+            SQLTempDBDir          = "${tempdbdriveletter}:\Program Files\Microsoft SQL Server\$SQLLocation.$SQLInstanceName\MSSQL\TempDb"
+            SQLTempDBLogDir       = "${tempdbdriveletter}:\Program Files\Microsoft SQL Server\$SQLLocation.$SQLInstanceName\MSSQL\TempDb"
+            SQLBackupDir          = "${datadriveletter}:\Program Files\Microsoft SQL Server\$SQLLocation.$SQLInstanceName\MSSQL\Backup"
+            SourcePath            = 'C:\SQLServerFull'
+            UpdateEnabled         = 'False'
+            ForceReboot           = $false
+            BrowserSvcStartupType = 'Automatic'
+
+            PsDscRunAsCredential  = $localAdminCreds
+
+            DependsOn             = '[Script]CleanSQL'
+        }
+
+        SqlServerMaxDop Set_SQLServerMaxDop_ToAuto
+        {
+            Ensure                  = 'Present'
+            DynamicAlloc            = $true
+            InstanceName            = $SQLInstanceName
+            PsDscRunAsCredential    = $localAdminCreds
+
+            DependsOn = '[SqlSetup]InstallNamedInstance'
+        }
+
+        SqlServerMemory Set_SQLServerMaxMemory_ToAuto
+        {
+            Ensure                  = 'Present'
+            DynamicAlloc            = $true
+            InstanceName            = $SQLInstanceName
+            PsDscRunAsCredential    = $localAdminCreds
+
+            DependsOn = '[SqlSetup]InstallNamedInstance'
+        }
+
+        SqlWindowsFirewall Create_FirewallRules
+        {
+            Ensure           = 'Present'
+            Features         = $SQLFeatures
+            InstanceName     = $SQLInstanceName
+            SourcePath       = 'C:\SQLServerFull'
+
+            DependsOn = '[SqlSetup]InstallNamedInstance'
+        }
+        
+        SqlAlwaysOnService 'EnableAlwaysOn'
+        {
+            Ensure               = 'Present'
+            ServerName           = 'LOCALHOST'
+            InstanceName         = 'MSSQLSERVER'
+            RestartTimeout       = 120
+
+            PsDscRunAsCredential = $localAdminCreds
+        }
+
+    }
+}
+
+
+function WaitForSqlSetup
+{
+    # Wait for SQL Server Setup to finish before proceeding.
+    while ($true)
+    {
+        try
+        {
+            Get-ScheduledTaskInfo "\ConfigureSqlImageTasks\RunConfigureImage" -ErrorAction Stop
+            Start-Sleep -Seconds 5
+        }
+        catch
+        {
+            break
+        }
+    }
+}
+
+$ConfigData = @{
+    AllNodes = @(
+    @{
+        NodeName = 'localhost'
+        PSDscAllowPlainTextPassword = $true
+    }
+    )
+}
+
+#$AdminCreds = Get-Credential
+#$SvcCreds = $AdminCreds
+ PrimaryAlwaysOnSQLServer -DomainName tamz.local -Admincreds $AdminCreds -SQLServicecreds $SvcCreds -Verbose -ConfigurationData $ConfigData -OutputPath d:\
+ Start-DscConfiguration -wait -Force -Verbose -Path D:\
