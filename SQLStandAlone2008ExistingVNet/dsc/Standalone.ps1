6f7aee2bfb94bd2f4f83bff839c6d6f653325f05
Troy Ault <aultt@users.noreply.github.com>
2019-04-08 13:26:57 -0400

    Adding2008Test

---
+++ SQLStandAlone2008ExistingVNet/dsc/Standalone.ps1
@@ -0,0 +1,203 @@
+
+configuration StandAlone
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$AdminCreds,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$SQLServicecreds,
+        [string]$imageoffer,
+        [string]$SQLFeatures,
+        [string]$SQLInstanceName,
+        [string]$datadriveLetter,
+        [string]$logdriveLetter,
+        [string]$tempdbdriveLetter,
+        [string]$SQLSysAdmins,
+        [string]$SourcePath,
+        [string]$SQLPort,
+        [string]$TimeZone,
+
+        [Int]$RetryCount = 20,
+        [Int]$RetryIntervalSec = 30
+
+
+    )
+
+    Import-DscResource -ModuleName ComputerManagementdsc, xPendingReboot, sqlserverdsc,Storagedsc,SecurityPolicydsc
+    $SQLVersion = $imageoffer.Substring(5,2)
+    $SQLLocation = "MSSQL$(switch ($SQLVersion){17 {14} 16 {13}})"
+
+    Node localhost
+    {
+        # Set LCM to reboot if needed
+        LocalConfigurationManager {
+            RebootNodeIfNeeded = $true
+            ActionafterReboot = 'ContinueConfiguration'
+        }
+        WaitForDisk DataVolume{
+            DiskId = 2
+            RetryIntervalSec = 60
+            RetryCount =60
+        }
+
+        Disk DataVolume{
+            DiskId =  2
+            DriveLetter = $datadriveLetter
+            FSFormat = 'NTFS'
+            AllocationUnitSize = 64kb
+            DependsOn = '[WaitForDisk]DataVolume'
+        }
+
+        WaitForDisk LogVolume{
+            DiskId = 3
+            RetryIntervalSec = 60
+            RetryCount =60
+        }
+
+        Disk LogVolume{
+            DiskId =  3
+            DriveLetter = $logdriveLetter
+            FSFormat = 'NTFS'
+            AllocationUnitSize = 64kb
+            DependsOn = '[WaitForDisk]LogVolume'
+        }
+
+        PowerPlan HighPerf
+        {
+          IsSingleInstance = 'Yes'
+          Name             = 'High performance'
+        }
+
+        TimeZone SetTimeZone
+        {
+            IsSingleInstance = 'Yes'
+            TimeZone         = $TimeZone
+        }
+
+        Computer DomainJoin
+        {
+            Name       = $env:COMPUTERNAME
+            DomainName = $DomainName
+            Credential = $AdminCreds
+        }
+
+        Script CleanSQL
+        {
+            SetScript  = 'C:\SQLServerFull\Setup.exe /Action=Uninstall /FEATURES=SQL,AS,RS,IS /INSTANCENAME=MSSQLSERVER /Q'
+            TestScript = "(test-path -Path `"C:\Program Files\Microsoft SQL Server\$SQLLocation.MSSQLSERVER\MSSQL\DATA\master.mdf`") -eq `$false"
+            GetScript  = "@{Ensure = if ((test-path -Path `"C:\Program Files\Microsoft SQL Server\$SQLLocation.MSSQLSERVER\MSSQL\DATA\master.mdf`") -eq `$false) {'Present'} Else {'Absent'}}"
+            DependsON = '[Computer]DomainJoin'
+        }
+
+        xPendingReboot Reboot1
+        {
+            Name = 'Reboot1'
+            dependson = '[Script]CleanSQL'
+        }
+
+        SqlSetup 'InstallNamedInstance'
+        {
+            InstanceName          = $SQLInstanceName
+            Features              = $SQLFeatures
+            SQLCollation          = 'SQL_Latin1_General_CP1_CI_AS'
+            SQLSysAdminAccounts   = $SQLSysAdmins
+            SQLSvcAccount         = $SQLServicecreds
+            InstallSharedDir      = 'C:\Program Files\Microsoft SQL Server'
+            InstallSharedWOWDir   = 'C:\Program Files (x86)\Microsoft SQL Server'
+            InstanceDir           = "${datadriveletter}:\Program Files\Microsoft SQL Server"
+            InstallSQLDataDir     = "${datadriveletter}:\Program Files\Microsoft SQL Server\"
+            SQLUserDBDir          = "${datadriveletter}:\Program Files\Microsoft SQL Server\$SQLLocation.$SQLInstanceName\MSSQL\Data"
+            SQLUserDBLogDir       = "${logdriveletter}:\Program Files\Microsoft SQL Server\$SQLLocation.$SQLInstanceName\MSSQL\Log"
+            SQLTempDBDir          = "${tempdbdriveletter}:\Program Files\Microsoft SQL Server\$SQLLocation.$SQLInstanceName\MSSQL\TempDb"
+            SQLTempDBLogDir       = "${logdriveletter}:\Program Files\Microsoft SQL Server\$SQLLocation.$SQLInstanceName\MSSQL\TempDb"
+            SQLBackupDir          = "${datadriveletter}:\Program Files\Microsoft SQL Server\$SQLLocation.$SQLInstanceName\MSSQL\Backup"
+            SourcePath            = $SourcePath
+            UpdateEnabled         = 'False'
+            ForceReboot           = $false
+            BrowserSvcStartupType = 'Automatic'
+
+            PsDscRunAsCredential  = $AdminCreds
+
+            DependsOn             = '[xPendingReboot]Reboot1','[Disk]LogVolume','[Disk]DataVolume'
+        }
+
+        UserRightsAssignment PerformVolumeMaintenanceTasks
+        {
+            Policy = "Perform_volume_maintenance_tasks"
+            Identity = $SQLServicecreds.UserName
+
+            DependsOn  = '[Computer]DomainJoin'
+        }
+
+        UserRightsAssignment LockPagesInMemory
+        {
+            Policy = "Lock_pages_in_memory"
+            Identity = $SQLServicecreds.UserName
+
+            DependsOn  = '[Computer]DomainJoin'
+        }
+
+        SqlServerNetwork 'ChangeTcpIpOnDefaultInstance'
+        {
+            InstanceName         = $SQLInstanceName
+            ProtocolName         = 'Tcp'
+            IsEnabled            = $true
+            TCPDynamicPort       = $false
+            TCPPort              = $SQLPort
+            RestartService       = $true
+            DependsOn = '[SqlSetup]InstallNamedInstance'
+            
+            PsDscRunAsCredential = $AdminCreds
+        }
+
+        SqlServerMaxDop Set_SQLServerMaxDop_ToAuto
+        {
+            Ensure                  = 'Present'
+            DynamicAlloc            = $true
+            InstanceName            = $SQLInstanceName
+            PsDscRunAsCredential    = $AdminCreds
+
+            DependsOn = '[SqlSetup]InstallNamedInstance'
+        }
+
+        SqlServerMemory Set_SQLServerMaxMemory_ToAuto
+        {
+            Ensure                  = 'Present'
+            DynamicAlloc            = $true
+            InstanceName            = $SQLInstanceName
+            PsDscRunAsCredential    = $AdminCreds
+
+            DependsOn = '[SqlSetup]InstallNamedInstance'
+        }
+
+        SqlWindowsFirewall Create_FirewallRules
+        {
+            Ensure           = 'Present'
+            Features         = $SQLFeatures
+            InstanceName     = $SQLInstanceName
+            SourcePath       = $SourcePath
+
+            DependsOn = '[SqlSetup]InstallNamedInstance'
+        }
+
+    }
+}
+ 
+$ConfigData = @{
+    AllNodes = @(
+    @{
+        NodeName = 'localhost'
+        PSDscAllowPlainTextPassword = $true
+    }
+    )
+}
+
+#  $AdminCreds = Get-Credential
+# $SQLServicecreds = $AdminCreds
+# StandAlone -DomainName tamz.local -domainuserCreds $AdminCreds -SQLServicecreds $SQLServicecreds -imageOffer "SQL2017-WS2016" -SQLFeatures "SQLENGINE" -SQLInstance "MSSQLSERVER" -datadriveLetter "F" -logdriveLetter "L" -tempdbdriveletter "F" -SQLSysAdmins "TAMZ\DBA" -SourcePath "C:\\SQLServerFull" -SQLPort 1433 -TimeZone "Eastern Standard Time" -Verbose -ConfigurationData $ConfigData -OutputPath d:\
+# Start-DscConfiguration -wait -Force -Verbose -Path D:\
